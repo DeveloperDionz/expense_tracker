5a971a490b44072f0863778bd28f2ad4c5d016e9
Dionysius Ogero <dionysiusdionz19@gmail.com>
2024-08-10 16:38:04 +0300

    My expense tracker

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 3000;
